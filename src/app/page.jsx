71118f74c14906884365e47bf9df1d0713b7e1de
edthxr <ritsongmueng@gmail.com>
2024-09-18 22:41:54 +0700

    fix responsive

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -130,7 +130,7 @@ export default function Home() {
 
           {/* Action Buttons */}
           <div className="flex flex-col lg:flex-row justify-between items-center my-2">
-          <div className="flex items-center mb-2 lg:mb-0 gap-3 sm:gap-0">
+          <div className="flex flex-col lg:flex-row items-center mb-2 lg:mb-0 gap-3 sm:gap-0">
               <img
                 src="/photo/channels4_profile.jpg"
                 alt="channel profile"
